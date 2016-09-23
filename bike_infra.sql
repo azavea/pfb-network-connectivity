a2320721e57de53fc5ebc60c58c95a1a6ecd0fde
Spencer Gardner <spencergardner@gmail.com>
2016-09-23 00:16:29 -0500

    lksdjf ;

--- bike_infra.sql
+++ bike_infra.sql
@@ -43,9 +43,12 @@ AND (
         OR  (COALESCE(one_way_car,'ft') = 'ft' AND osm.cycleway = 'buffered_lane')
         OR  (COALESCE(one_way_car,'ft') = 'ft' AND osm."cycleway:right" = 'buffered_lane')
         OR  (COALESCE(one_way_car,'ft') = 'ft' AND osm.cycleway = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
+        OR  (COALESCE(one_way_car,'ft') = 'ft' AND osm."cycleway:right" = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (COALESCE(one_way_car,'ft') = 'ft' AND osm."cycleway:right" = 'lane' AND osm."cycleway:right:buffer" IN ('yes','both','right','left'))
+        OR  (one_way_car = 'ft' AND osm."cycleway:left" = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'ft' AND osm."cycleway:left" = 'lane' AND osm."cycleway:left:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'tf' AND osm.cycleway = 'opposite_lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
+        OR  (one_way_car = 'tf' AND osm."cycleway:left" = 'opposite_lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'tf' AND osm."cycleway:left" = 'opposite_lane' AND osm."cycleway:left:buffer" IN ('yes','both','right','left'))
 );
 
@@ -106,9 +109,12 @@ AND (
         OR  (COALESCE(one_way_car,'tf') = 'tf' AND osm.cycleway = 'buffered_lane')
         OR  (COALESCE(one_way_car,'tf') = 'tf' AND osm."cycleway:left" = 'buffered_lane')
         OR  (COALESCE(one_way_car,'tf') = 'tf' AND osm.cycleway = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
+        OR  (COALESCE(one_way_car,'tf') = 'tf' AND osm."cycleway:left" = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (COALESCE(one_way_car,'tf') = 'tf' AND osm."cycleway:left" = 'lane' AND osm."cycleway:left:buffer" IN ('yes','both','right','left'))
+        OR  (one_way_car = 'tf' AND osm."cycleway:right" = 'lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'tf' AND osm."cycleway:right" = 'lane' AND osm."cycleway:right:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'ft' AND osm.cycleway = 'opposite_lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
+        OR  (one_way_car = 'ft' AND osm."cycleway:right" = 'opposite_lane' AND osm."cycleway:buffer" IN ('yes','both','right','left'))
         OR  (one_way_car = 'ft' AND osm."cycleway:right" = 'opposite_lane' AND osm."cycleway:right:buffer" IN ('yes','both','right','left'))
 );
 
