d9dbcdfd8fef8dd23f3a63e700c62cedc08085cc
Reed Martz <rmartz@azavea.com>
2016-10-31 14:19:35 -0400

    Add indexes and tweak INSERT query performance
    Testing indicates Census Connected Blocks insert query should
    successfully run in < 10 minutes (Compared to 2.5 hours previously).

--- connectivity/census_blocks.sql
+++ connectivity/census_blocks.sql
@@ -16,4 +16,5 @@ ALTER TABLE neighborhood_census_blocks ADD COLUMN IF NOT EXISTS rec_low_stress I
 ALTER TABLE neighborhood_census_blocks ADD COLUMN IF NOT EXISTS rec_high_stress INT;
 
 CREATE INDEX IF NOT EXISTS idx_neighborhood_blocks10 ON neighborhood_census_blocks (blockid10);
-ANALYZE neighborhood_census_blocks (blockid10);
+CREATE INDEX IF NOT EXISTS idx_neighborhood_geom ON neighborhood_census_blocks USING GIST (geom);
+ANALYZE neighborhood_census_blocks;
