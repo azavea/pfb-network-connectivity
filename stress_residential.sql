7f14cdd6a999841cb21248996fd4a738b1e1088f
Spencer <spencergardner@gmail.com>
2016-11-15 10:41:44 -0600

    residential scoring tweaks to treat like tertiary if multiple lanes or high speed

--- stress_residential.sql
+++ stress_residential.sql
@@ -7,7 +7,10 @@
 UPDATE  cambridge_ways SET ft_seg_stress = NULL, tf_seg_stress = NULL
 WHERE   functional_class = 'residential'
 AND     (ft_bike_infra IS NULL OR ft_bike_infra NOT IN ('track','buffered_lane','lane'))
-AND     (tf_bike_infra IS NULL OR tf_bike_infra NOT IN ('track','buffered_lane','lane'));
+AND     (tf_bike_infra IS NULL OR tf_bike_infra NOT IN ('track','buffered_lane','lane'))
+AND     (ft_lanes IS NULL OR ft_lanes = 1)
+AND     (ft_lanes IS NULL OR tf_lanes = 1)
+AND     (speed_limit IS NULL OR speed_limit <= 30);
 
 -- no additional information
 UPDATE  cambridge_ways
@@ -15,7 +18,10 @@ SET     ft_seg_stress = 1,
         tf_seg_stress = 1
 WHERE   functional_class = 'residential'
 AND     (ft_bike_infra IS NULL OR ft_bike_infra NOT IN ('track','buffered_lane','lane'))
-AND     (tf_bike_infra IS NULL OR tf_bike_infra NOT IN ('track','buffered_lane','lane'));
+AND     (tf_bike_infra IS NULL OR tf_bike_infra NOT IN ('track','buffered_lane','lane'))
+AND     (ft_lanes IS NULL OR ft_lanes = 1)
+AND     (ft_lanes IS NULL OR tf_lanes = 1)
+AND     (speed_limit IS NULL OR speed_limit <= 30);
 
 -- stress increase for narrow one way and parking on both sides
 UPDATE  cambridge_ways
@@ -24,6 +30,9 @@ SET     ft_seg_stress = 2,
 WHERE   functional_class = 'residential'
 AND     (ft_bike_infra IS NULL OR ft_bike_infra NOT IN ('track','buffered_lane','lane'))
 AND     (tf_bike_infra IS NULL OR tf_bike_infra NOT IN ('track','buffered_lane','lane'))
+AND     (ft_lanes IS NULL OR ft_lanes = 1)
+AND     (ft_lanes IS NULL OR tf_lanes = 1)
+AND     (speed_limit IS NULL OR speed_limit <= 30)
 AND     width_ft <= 26
 AND     ft_park = 1
 AND     tf_park = 1;
