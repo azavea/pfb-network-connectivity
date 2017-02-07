45e297e99af541e48f93b98aacab1923d35f69de
Kathryn Killebrew <kkillebrew@azavea.com>
2017-02-07 12:59:05 -0500

    Convert bounds to WGS84
    Handle arbitrary input projections; tidy formatting.

--- detect_utm_zone.py
+++ detect_utm_zone.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 """
-Accepts a polygon shapefile in WGS84 and finds the SRID for the UTM zone
+Accepts a polygon shapefile and finds the SRID for the UTM zone
 Based on https://github.com/jbranigan/geo-scripts-python/blob/master/latlng2utm/detect-utm-zone.py
 """
 
-from osgeo import ogr
-import math
 import argparse
+import math
+from osgeo import ogr, osr
+import sys
 
 
 def check_latlng(check_bbox):
-    ''' Checks to see if the file coordinates are in lat/lng '''
+    """ Checks to see if the file coordinates are in lat/lng """
     for i in check_bbox:
         if i < -180 or i > 180:
             failure('This file is already projected.')
@@ -19,7 +20,7 @@ def check_latlng(check_bbox):
 
 
 def check_width(check_bbox):
-    ''' Checsk to see if the bounding box fits in a UTM zone '''
+    """ Checks to see if the bounding box fits in a UTM zone """
     wide = check_bbox[1] - check_bbox[0]
     if wide > 3:
         failure('This file is too many degrees wide for UTM')
@@ -27,7 +28,7 @@ def check_width(check_bbox):
 
 
 def get_zone(coord):
-    ''' Finds the UTM zone of the coordinate '''
+    """ Finds the UTM zone of a WGS84 coordinate """
     # There are 60 longitudinal projection zones numbered 1 to 60 starting at 180W
     # So that's -180 = 1, -174 = 2, -168 = 3
     zone = ((coord - -180) / 6.0)
@@ -35,7 +36,9 @@ def get_zone(coord):
 
 
 def get_bbox(shapefile):
-    ''' Gets the bounding box of a shapefile input '''
+    """ Gets the bounding box of a shapefile in EPSG 4326.
+        If shapefile is not in WGS84, bounds are reprojected.
+    """
     driver = ogr.GetDriverByName('ESRI Shapefile')
     # 0 means read, 1 means write
     data_source = driver.Open(shapefile, 0)
@@ -45,13 +48,29 @@ def get_bbox(shapefile):
     else:
         layer = data_source.GetLayer()
         shape_bbox = layer.GetExtent()
+
+        spatialRef = layer.GetSpatialRef()
+        target = osr.SpatialReference()
+        target.ImportFromEPSG(4326)
+
+        # this check for non-WGS84 projections gets some false positives, but that's okay
+        if target.ExportToProj4() != spatialRef.ExportToProj4():
+            transform = osr.CoordinateTransformation(spatialRef, target)
+            point1 = ogr.Geometry(ogr.wkbPoint)
+            point1.AddPoint(shape_bbox[0], shape_bbox[2])
+            point2 = ogr.Geometry(ogr.wkbPoint)
+            point2.AddPoint(shape_bbox[1], shape_bbox[3])
+            point1.Transform(transform)
+            point2.Transform(transform)
+            shape_bbox = [point1.GetX(), point2.GetX(), point1.GetY(), point2.GetY()]
+
         return shape_bbox
 
 
 def failure(why):
-    ''' Quits the script with an exit message '''
-    print why
-    raise SystemExit
+    """ Quits the script with an exit message """
+    print(why)
+    sys.exit(1)
 
 
 def get_srid(filename):
