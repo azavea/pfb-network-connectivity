341a2c8ae610b61555e8e4b9753cced66ae35852
Sharp Hall <shall@azavea.com>
2017-02-08 11:54:27 -0500

    Add intelligent wait for database

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 NB_POSTGRESQL_DB=pfb
@@ -5,11 +7,36 @@ NB_POSTGRESQL_USER=gis
 NB_POSTGRESQL_PASSWORD=gis
 
 # start postgres and capture the PID
-/docker-entrypoint.sh postgres &
+/docker-entrypoint.sh postgres | tee /tmp/postgres_stdout.txt &
 POSTGRES_PROC=$!
 
-# sleep while db comes up
-sleep 10
+MAX_TRIES=30
+counter=1
+
+# wait for database to become available
+while true
+do
+    echo waiting for database, try ${counter}
+    sleep 3
+    # don't check if postgres is up until docker-entrypoint gives us the signal
+    if grep 'PostgreSQL init process complete' /tmp/postgres_stdout.txt > /dev/null
+    then
+        set +e
+        psql -U "${NB_POSTGRESQL_USER}" -d "${NB_POSTGRESQL_DB}" -c "SELECT 1" &> /dev/null
+        postgresql_status=$?
+        set -e
+        if [ "$postgresql_status" == "0" ]
+        then
+            break
+        fi
+    fi
+    if [ "$counter" == "$MAX_TRIES" ]; then
+        echo database did not come up successfully
+        kill $POSTGRES_PROC
+        exit 1
+    fi
+    ((counter++))
+done
 
 # run job
 cd /pfb
