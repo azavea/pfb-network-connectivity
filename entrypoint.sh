63aeaf1f26162a38d0f18987670e4f6e88f32a59
Sharp Hall <shall@azavea.com>
2017-02-08 10:54:42 -0500

    Consolidate build inside of Dockerfile
    Move database initialization to /docker-entrypoint-initdb.d/

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,25 +11,6 @@ POSTGRES_PROC=$!
 # sleep while db comes up
 sleep 10
 
-# set up database
-su postgres bash -c psql <<EOF
-CREATE USER gis WITH PASSWORD 'gis';
-ALTER USER gis WITH SUPERUSER;
-CREATE DATABASE pfb WITH OWNER gis ENCODING 'UTF-8';
-EOF
-
-# install extensions
-psql -U "${NB_POSTGRESQL_USER}" -d "${NB_POSTGRESQL_DB}" <<EOF
-CREATE EXTENSION "postgis";
-CREATE EXTENSION "uuid-ossp";
-CREATE EXTENSION "hstore";
-CREATE EXTENSION "plpythonu";
-CREATE EXTENSION "pgrouting";
-CREATE EXTENSION "quantile";
-CREATE EXTENSION "tdg";
-ALTER USER gis SET search_path TO generated,received,scratch,"\$user",tdg,public;
-EOF
-
 # run job
 cd /pfb
 
