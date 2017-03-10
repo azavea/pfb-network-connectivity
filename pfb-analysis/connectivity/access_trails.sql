f0c8a6988fb14b2054f6a668634caad7eed3ab70
Spencer <spencergardner@gmail.com>
2017-03-10 17:05:24 -0600

    Add road ids to paths. Add calculation for access to paths. Remove unneeded connected_census_blocks_recreation

--- pfb-analysis/connectivity/access_trails.sql
+++ pfb-analysis/connectivity/access_trails.sql
@@ -1,45 +1,49 @@
--- ----------------------------------------
--- -- INPUTS
--- -- location: neighborhood
--- ----------------------------------------
--- -- low stress access
--- UPDATE  neighborhood_census_blocks
--- SET     trails_low_stress = (
---             SELECT  COUNT(path_id)
---             FROM    neighborhood_paths
---             WHERE   EXISTS (
---                         SELECT  1
---                         FROM    neighborhood_census_block_roads cbr,
---                                 neighborhood_reachable_roads_low_stress ls,
---                                 neighborhood_ways,
---                                 neighborhood_paths
---                         WHERE   cb.source_blockid10 = neighborhood_census_blocks.blockid10
---                         AND     cb.target_blockid10 = blocks2.blockid10
---                         AND     cb.low_stress
---             )
---         ),
---         trails_high_stress = (
---             SELECT  COUNT(path_id)
---             FROM    neighborhood_paths
---             WHERE   EXISTS (
---                         SELECT  1
---                         FROM    neighborhood_census_block_roads cbr,
---                                 neighborhood_reachable_roads_low_stress ls,
---                                 neighborhood_ways,
---                                 neighborhood_paths
---                         WHERE   cb.source_blockid10 = neighborhood_census_blocks.blockid10
---                         AND     cb.target_blockid10 = blocks2.blockid10
---             )
---         )
--- WHERE   EXISTS (
---             SELECT  1
---             FROM    neighborhood_boundary AS b
---             WHERE   ST_Intersects(neighborhood_census_blocks.geom,b.geom)
---         );
---
--- -- set block-based ratio
--- UPDATE  neighborhood_census_blocks
--- SET     trails_ratio = CASE WHEN trails_high_stress IS NULL THEN NULL
---                             WHEN trails_high_stress = 0 THEN 0
---                             ELSE trails_low_stress::FLOAT / trails_high_stress
---                             END;
+----------------------------------------
+-- INPUTS
+-- location: neighborhood
+-- :min_path_length and :min_path_bbox must
+-- be set before running this script
+--       e.g. psql -v nb_output_srid=4326 -v min_path_length=15840 -v min_bbox_length=10560 -f community_centers.sql
+----------------------------------------
+-- low stress access
+UPDATE  neighborhood_census_blocks
+SET     trails_low_stress = (
+            SELECT  COUNT(path_id)
+            FROM    neighborhood_paths
+            WHERE   path_length > :min_path_length
+            AND     bbox_length > :min_bbox_length
+            AND     EXISTS (
+                        SELECT  1
+                        FROM    neighborhood_reachable_roads_low_stress ls
+                        JOIN    neighborhood_census_block_roads cbr
+                                ON ls.base_road = cbr.road_id
+                                AND neighborhood_census_blocks.blockid10 = cbr.blockid10
+                        WHERE   ls.target_road = ANY(neighborhood_paths.road_ids)
+            )
+        ),
+        trails_high_stress = (
+            SELECT  COUNT(path_id)
+            FROM    neighborhood_paths
+            WHERE   path_length > :min_path_length
+            AND     bbox_length > :min_bbox_length
+            AND     EXISTS (
+                        SELECT  1
+                        FROM    neighborhood_reachable_roads_high_stress hs
+                        JOIN    neighborhood_census_block_roads cbr
+                                ON hs.base_road = cbr.road_id
+                                AND neighborhood_census_blocks.blockid10 = cbr.blockid10
+                        WHERE   hs.target_road = ANY(neighborhood_paths.road_ids)
+            )
+        )
+WHERE   EXISTS (
+            SELECT  1
+            FROM    neighborhood_boundary AS b
+            WHERE   ST_Intersects(neighborhood_census_blocks.geom,b.geom)
+        );
+
+-- set block-based ratio
+UPDATE  neighborhood_census_blocks
+SET     trails_ratio = CASE WHEN trails_high_stress IS NULL THEN NULL
+                            WHEN trails_high_stress = 0 THEN 0
+                            ELSE trails_low_stress::FLOAT / trails_high_stress
+                            END;
