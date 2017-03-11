22e52cfda277c4d8bb72e506dd5be8810b29db94
Spencer Gardner <spencergardner@gmail.com>
2017-03-11 10:19:20 -0600

    Remove superfluous ANALYZE

--- pfb-analysis/connectivity/connected_census_blocks.sql
+++ pfb-analysis/connectivity/connected_census_blocks.sql
@@ -60,11 +60,8 @@ OR      (
                     END
         );
 
--- block pair index
+-- indexes
 CREATE UNIQUE INDEX idx_neighborhood_blockpairs ON neighborhood_connected_census_blocks (source_blockid10,target_blockid10);
-ANALYZE neighborhood_connected_census_blocks;
-
--- stress index
 CREATE INDEX IF NOT EXISTS idx_neighborhood_blockpairs_lstress ON neighborhood_connected_census_blocks (low_stress) WHERE low_stress IS TRUE;
 CREATE INDEX IF NOT EXISTS idx_neighborhood_blockpairs_hstress ON neighborhood_connected_census_blocks (high_stress) WHERE high_stress IS TRUE;
 ANALYZE neighborhood_connected_census_blocks;
