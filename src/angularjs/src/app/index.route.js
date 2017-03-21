9a06e3e0cd11bdedabdc37d058427f3955f646a1
Kathryn Killebrew <kkillebrew@azavea.com>
2017-03-21 14:34:03 -0400

    Add neighborhood view
    Add neighborhood list view to Angular app; add link in navbar.
    Connects #142.

--- src/angularjs/src/app/index.route.js
+++ src/angularjs/src/app/index.route.js
@@ -88,6 +88,17 @@
                 controller: 'AnalysisJobListController',
                 controllerAs: 'analysisJobList',
                 templateUrl: 'app/analysis-jobs/list/analysis-jobs-list.html'
+            })
+            .state('neighborhoods', {
+                abstract: true,
+                url: '/neighborhoods/?limit&offset',
+                template: '<ui-view/>'
+            })
+            .state('neighborhoods.list', {
+                url: '',
+                controller: 'NeighborhoodListController',
+                controllerAs: 'neighborhoodList',
+                templateUrl: 'app/neighborhoods/list/neighborhoods-list.html'
             });
         $urlRouterProvider.otherwise('/login/');
     }
