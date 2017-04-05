7db99f6fdc5f6ab595ef37019fe0718d5c14a9c3
Klaas Hoekema <khoekema@azavea.com>
2017-04-05 09:56:38 -0400

    Stay with shapefile, use ogrinfo to get extent
    Running NYC revealed that
    - The GeoJSON is really big--too much for some tools, really slow with
    others--so this changes to reproject but stick with shapefile format
    - One of the things that couldn't handle the huge json was the bbox
    finding command (both geojson-extent and turf/bbox crashed), so this
    switches to using ogrinfo.

--- src/tilemaker/entrypoint.sh
+++ src/tilemaker/entrypoint.sh
@@ -26,22 +26,21 @@ aws s3 cp "${TL_AWS_RESULTS_PATH}/${TL_SHAPEFILE_NAME}.zip" ./
 unzip "${TL_SHAPEFILE_NAME}.zip"
 
 mkdir -p /data
-ogr2ogr -t_srs EPSG:4326 -f GeoJSON "/data/${TL_SHAPEFILE_NAME}.json" \
+# Reproject. Could convert to GeoJSON as well, but shapefile is smaller and the tile conversion
+# can handle it just as well.
+ogr2ogr -t_srs EPSG:4326 -f "ESRI Shapefile" "/data/${TL_SHAPEFILE_NAME}.shp" \
     "${PFB_TEMPDIR}/${TL_SHAPEFILE_NAME}.shp"
 
-# Get bounds
-# If https://github.com/mapbox/geojson-extent/pull/7 gets merged, put geojson-extent back in
-# the Dockerfile npm list and switch to:
-# TL_BOUNDS=$(geojson-extent < "/data/${TL_SHAPEFILE_NAME}.json" spaces)
-npm link @turf/bbox
-TL_BOUNDS=$(node --eval \
-    "console.log(require('@turf/bbox')(JSON.parse(require('fs').readFileSync(process.argv.pop()))).join(' '));" \
-    "/data/${TL_SHAPEFILE_NAME}.json")
+# Get bounds. ogrinfo can handle big files, but doesn't provide tons of output flexibility, so
+# parse out the answer from the blob of text it returns.
+TL_BOUNDS=$(ogrinfo -so -al "/data/${TL_SHAPEFILE_NAME}.shp" \
+    | grep Extent | sed -E 's/.*\((.*), (.*)\) - \((.*), (.*)\).*/\1 \2 \3 \4/')
 
 # Make tiles and upload them to S3
+/usr/bin/time -f "\nTIMING: %C\nTIMING:\t%E elapsed %Kkb mem\n" \
 tl copy -z "${TL_MIN_ZOOM}" -Z "${TL_MAX_ZOOM}" -b "${TL_BOUNDS}" \
     "mapnik:///opt/tl-export/styles/${TL_SHAPEFILE_NAME}_style.xml" \
-    "${TL_AWS_RESULTS_PATH}/tiles/{z}/{x}/{y}.png?acl=public-read"
+    "${TL_AWS_RESULTS_PATH}/tiles/{z}/{x}/{y}.png"
 
 # Drop to a shell if run interactively
 bash
