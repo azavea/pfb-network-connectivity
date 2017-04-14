d91c97a82adbcd7337ab181df936a296d0193a89
Taylor Nation <tnation14@gmail.com>
2017-04-14 09:00:54 -0400

    Add IAM resources for a separate Batch container instance profile.
    - Separate Batch instance IAM permissions from those of ECS instances
      to ensure each service has minimum necessary permissions.
    
    - Terraform fmt changes

--- deployment/terraform/iam.tf
+++ deployment/terraform/iam.tf
@@ -32,9 +32,10 @@ data "aws_iam_policy_document" "ses_send_email" {
     effect = "Allow"
 
     resources = ["*"]
-    actions   = [
+
+    actions = [
       "ses:SendEmail",
-      "ses:SendRawEmail"
+      "ses:SendRawEmail",
     ]
   }
 }
@@ -89,7 +90,7 @@ resource "aws_iam_role_policy_attachment" "sqs_read_write" {
 }
 
 #
-# EC2 roles
+# App EC2 roles
 #
 resource "aws_iam_role" "container_instance_ec2" {
   name               = "${var.environment}ContainerInstanceProfile"
@@ -97,8 +98,8 @@ resource "aws_iam_role" "container_instance_ec2" {
 }
 
 resource "aws_iam_role_policy" "ec2_ses_send_email" {
-  name = "${var.environment}EC2SESSendEmail"
-  role = "${aws_iam_role.container_instance_ec2.id}"
+  name   = "${var.environment}EC2SESSendEmail"
+  role   = "${aws_iam_role.container_instance_ec2.id}"
   policy = "${data.aws_iam_policy_document.ses_send_email.json}"
 }
 
@@ -112,7 +113,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_logs_policy_container_inst
   policy_arn = "${var.aws_cloudwatch_logs_policy_arn}"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_s3_policy" {
+resource "aws_iam_role_policy_attachment" "app_ec2_s3_policy" {
   role       = "${aws_iam_role.container_instance_ec2.name}"
   policy_arn = "${var.aws_s3_policy_arn}"
 }
@@ -125,4 +126,27 @@ resource "aws_iam_role_policy_attachment" "ecs_for_ec2_policy_container_instance
 resource "aws_iam_instance_profile" "container_instance" {
   name  = "${aws_iam_role.container_instance_ec2.name}"
   roles = ["${aws_iam_role.container_instance_ec2.name}"]
+}
+
+#
+# Batch EC2 roles
+#
+resource "aws_iam_role" "batch_instance_ec2" {
+  name               = "${var.environment}BatchInstanceProfile"
+  assume_role_policy = "${data.aws_iam_policy_document.container_instance_ec2_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_for_ec2_policy_batch_instance_role" {
+  role       = "${aws_iam_role.batch_instance_ec2.name}"
+  policy_arn = "${var.aws_ecs_for_ec2_service_role_policy_arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "batch_ec2_s3_policy" {
+  role       = "${aws_iam_role.batch_instance_ec2.name}"
+  policy_arn = "${var.aws_s3_policy_arn}"
+}
+
+resource "aws_iam_instance_profile" "batch_instance" {
+  name  = "${aws_iam_role.batch_instance_ec2.name}"
+  roles = ["${aws_iam_role.batch_instance_ec2.name}"]
 }
