4d51b9682d5ac3a4b9b1466c4d3b40505d44773a
CloudNiner <andrew@cloudniner.co>
2017-04-19 11:12:57 -0400

    Clip OSM extract by census block extent
    Extent is equal to the extent of the neighborhood
    boundary + buffer (which equals max trip distance by default).
    
    This leads to smaller OSM graphs than the old method
    and will always contain the analysis area, which was not
    previously a guarantee.

--- src/analysis/import/import_osm.sh
+++ src/analysis/import/import_osm.sh
@@ -48,19 +48,9 @@ psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} \
 psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} \
   -c "DROP TABLE IF EXISTS scratch.neighborhood_cycwys_osm_way_types;"
 
-
-# Get the neighborhood_boundary bbox
-BBOX_SW_LNG=`psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} -t -c "SELECT MIN(ST_Xmin(ST_Transform(geom, 4326))) FROM neighborhood_boundary;"`
-BBOX_SW_LAT=`psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} -t -c "SELECT MIN(ST_Ymin(ST_Transform(geom, 4326))) FROM neighborhood_boundary;"`
-BBOX_NE_LNG=`psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} -t -c "SELECT MAX(ST_Xmax(ST_Transform(geom, 4326))) FROM neighborhood_boundary;"`
-BBOX_NE_LAT=`psql -h $NB_POSTGRESQL_HOST -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} -t -c "SELECT MAX(ST_Ymax(ST_Transform(geom, 4326))) FROM neighborhood_boundary;"`
-# Buffer it
-LNG_DIFF=`bc <<< "$BBOX_NE_LNG - $BBOX_SW_LNG"`
-LAT_DIFF=`bc <<< "$BBOX_NE_LAT - $BBOX_SW_LAT"`
-BBOX_SW_LAT=`bc <<< "$BBOX_SW_LAT - $LAT_DIFF"`
-BBOX_SW_LNG=`bc <<< "$BBOX_SW_LNG - $LNG_DIFF"`
-BBOX_NE_LAT=`bc <<< "$BBOX_NE_LAT + $LAT_DIFF"`
-BBOX_NE_LNG=`bc <<< "$BBOX_NE_LNG + $LNG_DIFF"`
+# Get the neighborhood_boundary bbox as extent of trimmed census blocks
+BBOX=$(psql -h ${NB_POSTGRESQL_HOST} -U ${NB_POSTGRESQL_USER} -d ${NB_POSTGRESQL_DB} -t -c "select ST_Extent(ST_Transform(geom, 4326)) from neighborhood_census_blocks;" | awk -F '[()]' '{print $2}' | tr " " ",")
+echo "CLIPPING OSM TO: ${BBOX}"
 
 OSM_TEMPDIR=`mktemp -d`
 
@@ -68,13 +58,13 @@ if [[ -f ${1} ]]; then
   update_status "IMPORTING" "Clipping provided OSM file"
   osmconvert "${1}" \
     --drop-broken-refs \
-    -b="${BBOX_SW_LNG}","${BBOX_SW_LAT}","${BBOX_NE_LNG}","${BBOX_NE_LAT}" \
+    -b="${BBOX}" \
     -o="${OSM_TEMPDIR}/converted.osm"
   OSM_DATA_FILE="${OSM_TEMPDIR}/converted.osm"
 else
   update_status "IMPORTING" "Downloading OSM data"
   # Download OSM data
-  OSM_API_URL="http://www.overpass-api.de/api/xapi?*[bbox=${BBOX_SW_LNG},${BBOX_SW_LAT},${BBOX_NE_LNG},${BBOX_NE_LAT}]"
+  OSM_API_URL="http://www.overpass-api.de/api/xapi?*[bbox=${BBOX}]"
   OSM_DATA_FILE="${OSM_TEMPDIR}/overpass.osm"
   wget -nv -O "${OSM_DATA_FILE}" "${OSM_API_URL}"
 fi
