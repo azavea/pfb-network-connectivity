f64a900c99c96d6345be52cb225c87b2f881b135
Kathryn Killebrew <kkillebrew@azavea.com>
2017-04-24 19:26:36 -0400

    Implement compare page
    Retrieve data for the places to compare and display in template.

--- src/angularjs/src/app/compare/compare.controller.js
+++ src/angularjs/src/app/compare/compare.controller.js
@@ -16,12 +16,8 @@
         initialize();
 
         function initialize() {
-            ctl.place = null;
-            ctl.lastJobScore = null;
-            ctl.jobResults = null;
+            ctl.places = [null, null, null];
             ctl.getPlace = getPlace;
-
-            ctl.downloads = null;
 
             $log.debug('place one: ');
             $log.debug($stateParams.place1);
@@ -29,20 +25,47 @@
             $log.debug($stateParams.place2);
             $log.debug('place three: ');
             $log.debug($stateParams.place3);
+
+            getPlace(0, $stateParams.place1);
+            getPlace(1, $stateParams.place2);
+            getPlace(2, $stateParams.place3);
         }
 
-        function getPlace(uuid) {
+        function getPlace(num, uuid) {
+            if (!uuid) {
+                return;
+            }
+
+            ctl.places[num] = {};
+
             Neighborhood.query({uuid: uuid}).$promise.then(function(data) {
-                ctl.place = new Neighborhood(data);
+                ctl.places[num].neighborhood = new Neighborhood(data);
             });
 
             AnalysisJob.query({neighborhood: uuid, latest: 'True'}).$promise.then(function(data) {
-
                 if (!data.results || !data.results.length) {
                     $log.warn('no matching analysis job found for neighborhood ' + uuid);
-                    ctl.lastJobScore = null;
-                    ctl.downloads = null;
+                    ctl.places[num] = null;
                     return;
+                }
+
+                var lastJob = new AnalysisJob(data.results[0]);
+                ctl.places[num].lastJob = lastJob;
+
+                if (lastJob) {
+                    AnalysisJob.results({uuid: lastJob.uuid}).$promise.then(function(results) {
+                        if (!results.overall_scores) {
+                            $log.warn('no job results found for neighborhood ' + lastJob.uuid);
+                            return;
+                        }
+
+                        ctl.places[num].jobResults = _.map(results.overall_scores, function(obj, key) {
+                            return {
+                                metric: key.replace(/_/g, ' '),
+                                score: obj.score_normalized
+                            };
+                        });
+                    });
                 }
             });
         }
