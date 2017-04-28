4f1576923ac9adfd4e44081393acf53e542dac91
Kathryn Killebrew <kkillebrew@azavea.com>
2017-04-28 13:48:42 -0400

    Use map constants
    Instead of hard-coding in map directives, use shared maps constants file.

--- src/angularjs/src/app/home/neighborhood-map.directive.js
+++ src/angularjs/src/app/home/neighborhood-map.directive.js
@@ -2,17 +2,17 @@
 (function() {
 
     /* @ngInject */
-    function NeighborhoodMapController(Neighborhood) {
+    function NeighborhoodMapController(MapConfig, Neighborhood) {
         var ctl = this;
         ctl.map = null;
 
         ctl.$onInit = function () {
             ctl.mapOptions = { scrollWheelZoom: false };
-            ctl.boundsConus = [[24.396308, -124.848974], [49.384358, -66.885444]];
+            ctl.boundsConus = MapConfig.conusBounds;
             ctl.baselayer = L.tileLayer(
-                'https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-                    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-                    maxZoom: 18
+                MapConfig.baseLayers.Stamen.url, {
+                    attribution: MapConfig.baseLayers.Stamen.attribution,
+                    maxZoom: MapConfig.conusMaxZoom
                 });
         };
 
--- src/angularjs/src/app/places/detail/place-map.directive.js
+++ src/angularjs/src/app/places/detail/place-map.directive.js
@@ -1,7 +1,7 @@
 (function() {
 
     /* @ngInject */
-    function PlaceMapController($filter, $http, $sanitize) {
+    function PlaceMapController($filter, $http, $sanitize, MapConfig) {
         var ctl = this;
         ctl.map = null;
         ctl.layerControl = null;
@@ -14,9 +14,9 @@
             // TODO: set center and zoom level by zooming to fit geojson polygon bounds
             ctl.mapCenter = [39.963277, -75.142971];
             ctl.baselayer = L.tileLayer(
-                'https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
-                    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
-                    maxZoom: 18
+                MapConfig.baseLayers.Stamen.url, {
+                    attribution: MapConfig.baseLayers.Stamen.attribution,
+                    maxZoom: MapConfig.conusMaxZoom
                 });
         };
 
