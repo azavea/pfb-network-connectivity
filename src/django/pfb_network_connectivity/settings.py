3c507f46b166bb806c8050a3eba303f8a438a6f8
CloudNiner <andrew@cloudniner.co>
2017-04-28 09:22:58 -0400

    Cleanup django-storages settings

--- src/django/pfb_network_connectivity/settings.py
+++ src/django/pfb_network_connectivity/settings.py
@@ -243,11 +243,9 @@ WATCHMAN_CHECKS = (
 # https://github.com/jschneier/django-storages
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_AUTO_CREATE_BUCKET = True
 AWS_STORAGE_BUCKET_NAME = os.getenv('PFB_S3_STORAGE_BUCKET',
                                     '{0}-pfb-storage-{1}'.format(DEV_USER, AWS_REGION))
 AWS_QUERYSTRING_AUTH = False
-AWS_QUERYSTRING_EXPIRE = 3600 * 24 * 14     # Two weeks
 
 
 # Email
