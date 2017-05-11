e952e02a16036500a89268955a9e53c78662e39c
Kathryn Killebrew <kkillebrew@azavea.com>
2017-05-11 16:08:34 -0400

    Fix fontello not loading
    Load fontello in app. Closes #413.

--- src/angularjs/gulp/build.js
+++ src/angularjs/gulp/build.js
@@ -50,7 +50,8 @@ gulp.task('html', ['inject', 'partials'], function () {
     .pipe(jsFilter.restore)
     .pipe(cssFilter)
     .pipe($.sourcemaps.init())
-    .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
+    .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/',
+                    '../assets/fonts/bootstrap/'))
     .pipe($.minifyCss({ processImport: false }))
     .pipe($.sourcemaps.write('maps'))
     .pipe(cssFilter.restore)
@@ -97,26 +98,12 @@ gulp.task('favicons', function () {
 //fonts are placed in /assets/fonts
 gulp.task('fonts', function () {
     return gulp.src(path.join(conf.paths.src, '/assets/fonts/**/*'))
-    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
-    .pipe($.flatten())
-    .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/bootstrap/')));
-});
-
-gulp.task('other', function () {
-  var fileFilter = $.filter(function (file) {
-    return file.stat.isFile();
-  });
-
-  return gulp.src([
-    path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss,jpg,png,gif,svg}')
-  ])
-    .pipe(fileFilter)
-    .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2,css}'))
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/assets/fonts/')));
 });
 
 gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'images', 'leaflet', 'favicons', 'fonts', 'other']);
+gulp.task('build', ['html', 'images', 'leaflet', 'favicons', 'fonts']);
