5ca5eb8ffb5771e9e7e7d5000f26efba0b8772a7
Kathryn Killebrew <kkillebrew@azavea.com>
2017-07-03 15:17:39 -0400

    Hide more from OrgAdmins
    Do not allow OrgAdmins to view full admins or users outside of its org (already could not modify them).
    Do not allow OrgAdmins to view other orgs (already could not modify them).
    
    Closes #408.

--- src/django/pfb_network_connectivity/filters.py
+++ src/django/pfb_network_connectivity/filters.py
@@ -9,7 +9,7 @@ from rest_framework import filters
 
 from pfb_network_connectivity.permissions import is_admin_org, is_admin
 from pfb_analysis.models import AnalysisJob
-from users.models import Organization
+from users.models import Organization, PFBUser, UserRoles
 
 
 logger = logging.getLogger(__name__)
@@ -30,22 +30,33 @@ class OrgAutoFilterBackend(filters.BaseFilterBackend):
 
 
 class OrgOrAdminAutoFilterBackend(filters.BaseFilterBackend):
-    """Filter that allows non-admin users to see only their own organization's objects."""
+    """Filter that allows non-admin users to see only their own organization's objects.
+
+    Non suer-admin users cannot see admin users.
+    """
 
     def filter_queryset(self, request, queryset, view):
-        if not hasattr(request.user, 'organization') or is_admin_org(request.user):
+        if not hasattr(request.user, 'organization') or is_admin(request.user):
             return queryset
         elif queryset.model == Organization:
             return queryset.filter(uuid=request.user.organization_id)
+        elif queryset.model == PFBUser:
+            queryset = queryset.filter(organization=request.user.organization)
+            return queryset.exclude(role=UserRoles.ADMIN)
         else:
             return queryset.filter(organization=request.user.organization)
 
 
 class SelfUserAutoFilterBackend(filters.BaseFilterBackend):
-    """Filter used on users endpoint to limit queryset to only user if user is not admin."""
+    """Filter used on users endpoint to limit queryset to only user if user is not admin.
+
+    Org admins cannot see full admins.
+    """
 
     def filter_queryset(self, request, queryset, view):
-        if not hasattr(request.user, 'organization') or is_admin_org(request.user):
+        if not hasattr(request.user, 'organization') or is_admin(request.user):
             return queryset
+        elif is_admin_org(request.user):
+            return queryset.exclude(role=UserRoles.ADMIN)
         else:
             return queryset.filter(uuid=request.user.uuid)
