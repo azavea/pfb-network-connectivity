fa81b24eafd06c45cded63c8d13986ac704bac0e
CloudNiner <andrew@cloudniner.co>
2017-07-12 11:54:10 -0400

    Update tile styles for ways and census blocks layers

--- src/angularjs/src/app/components/map/map.constants.js
+++ src/angularjs/src/app/components/map/map.constants.js
@@ -33,7 +33,7 @@
     legends: {
         census_blocks: {
             position: 'bottomright',
-            colors: ['#e2231a', '#c92433', '#b1264d', '#982766', '#802980', '#664396', '#4c5eac', '#3378c2', '#1993d8', '#00aeef'],
+            colors: ['#FF3300', '#D04628', '#B9503C', '#A25A51', '#8B6465', '#736D79', '#5C778D', '#4581A2', '#2E8BB6', '#009FDF'],
             labels: ['0 - 6', '6 - 12', '12 - 18', '18 - 24', '24 - 30', '30 - 36', '36 - 42', '42 - 48', '48 - 54', '54 - 100'],
             title: 'BNA Score'
         },
@@ -45,7 +45,7 @@
         },
         ways: {
             position: 'bottomright',
-            colors: ['#00aeef', '#e2231a'],
+            colors: ['#009fdf', '#ff3300'],
             labels: ['Low Stress', 'High Stress']
         }
     }
