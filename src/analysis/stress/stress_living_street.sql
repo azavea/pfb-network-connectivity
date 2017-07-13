a68204553e9a07c80434f29e47c005fe22c8d193
Spencer <spencergardner@gmail.com>
2017-07-13 14:13:58 -0500

    Add high stress on living streets where bike access is not allowed

--- src/analysis/stress/stress_living_street.sql
+++ src/analysis/stress/stress_living_street.sql
@@ -6,6 +6,15 @@ UPDATE  neighborhood_ways SET ft_seg_stress = NULL, tf_seg_stress = NULL
 WHERE   functional_class = 'living_street';
 
 UPDATE  neighborhood_ways
-SET     ft_seg_stress = 1,
-        tf_seg_stress = 1
+SET     ft_seg_stress = 3,
+        tf_seg_stress = 3
+FROM    neighborhood_osm_full_line osm
+WHERE   functional_class = 'living_street'
+AND     neighborhood_ways.osm_id = osm.osm_id
+AND     osm.bicycle = 'no';
+
+UPDATE  neighborhood_ways
+SET     ft_seg_stress = COALESCE(ft_seg_stress,1),
+        tf_seg_stress = COALESCE(tf_seg_stress,1)
+FROM    neighborhood_osm_full_line osm
 WHERE   functional_class = 'living_street';
