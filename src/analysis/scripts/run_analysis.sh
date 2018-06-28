5c6f0bd094d21d7ff3716d85f22292bc58d56dc9
CloudNiner <andrew@cloudniner.co>
2018-06-28 10:47:30 -0400

    Simplify PGPASSWORD var

--- src/analysis/scripts/run_analysis.sh
+++ src/analysis/scripts/run_analysis.sh
@@ -5,7 +5,6 @@ set -e
 export NB_POSTGRESQL_HOST="${NB_POSTGRESQL_HOST:-localhost}"
 export NB_POSTGRESQL_DB="${NB_POSTGRESQL_DB:-pfb}"
 export NB_POSTGRESQL_USER="${NB_POSTGRESQL_USER:-gis}"
-export NB_POSTGRESQL_PASSWORD="${NB_POSTGRESQL_PASSWORD:-gis}"
 
 # NB_MAX_TRIP_DISTANCE should be in the same units of the NB_OUTPUT_SRID projection
 # Typically meters because we autodetect and use UTM zones
