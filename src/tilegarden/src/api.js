6cfba32c0ca395f84d7525db4008752dad4c6ded
Klaas Hoekema <khoekema@azavea.com>
2018-11-30 15:11:16 -0500

    Remove vector and grid tile code
    We're not going to be using utfGrids or vector tiles here, so there's
    no reason to keep the code around in this repo (we could always copy it
    back in if we change our mind).

--- src/tilegarden/src/api.js
+++ src/tilegarden/src/api.js
@@ -4,20 +4,11 @@
 
 const APIBuilder = require('claudia-api-builder')
 
-const { imageTile, utfGrid, vectorTile, createMap } = require('./tiler')
+const { imageTile, createMap } = require('./tiler')
 const HTTPError = require('./util/error-builder')
 
 const IMAGE_HEADERS = {
     'Content-Type': 'image/png',
-}
-
-const UTF_HEADERS = {
-    'Content-Type': 'application/json',
-}
-
-const VECTOR_HEADERS = {
-    'Content-Encoding': 'gzip',
-    'Content-Type': 'application/vnd.mapbox-vector-tile',
 }
 
 const HTML_RESPONSE = { success: { contentType: 'text/html' } }
@@ -38,19 +29,6 @@ const processCoords = (req) => {
         throw HTTPError('Error: Coordinate values must be numbers!', 400)
     }
     return { z, x, y }
-}
-
-// Makes sure utf fields exist and returns them in the correct format
-const processUTFQuery = (req) => {
-    if (req.queryString.utf || req.queryString.fields || req.queryString.grid) {
-        /* eslint-disable-next-line quotes */
-        throw HTTPError("Invalid argument, did you mean '&utfFields='?", 400)
-    }
-
-    const queryString = req.queryString.utfFields
-    if (!queryString) throw HTTPError('Error: UTF grid missing field query!', 400)
-
-    return queryString.split(',')
 }
 
 // Returns a properly formatted list of layers
@@ -104,74 +82,19 @@ api.get(
     { success: { contentHandling: 'CONVERT_TO_BINARY' } },
 )
 
-// Get utf grid for some zxy bounds
-// in the original implementation this alone uses cors: why?
-api.get(
-    '/grid/{z}/{x}/{y}',
-    (req) => {
-        try {
-            const { z, x, y } = processCoords(req)
-            const utfFields = processUTFQuery(req)
-            const layers = processLayers(req)
-            const configOptions = processConfig(req)
-
-            return utfGrid(createMap(z, x, y, layers, configOptions), utfFields)
-                .then(g => new APIBuilder.ApiResponse(g, UTF_HEADERS, 200))
-                .catch(handleError)
-        } catch (e) {
-            return handleError(e)
-        }
-    },
-)
-// Catch this error because I keep doing it myself
-api.get(
-    '/utf/{z}/{x}/{y}',
-    () => new APIBuilder.ApiResponse(
-        /* eslint-disable-next-line quotes */
-        { message: "Invalid path, did you mean '/grid/{z}/{x}/{y}'?" },
-        { 'Content-Type': 'application/json' },
-        404,
-    ),
-)
-
-// Get a vector tile for some zxy bounds
-api.get(
-    '/vector/{z}/{x}/{y}',
-    (req) => {
-        try {
-            const { z, x, y } = processCoords(req)
-            const layers = processLayers(req)
-            const configOptions = processConfig(req)
-
-            return vectorTile(createMap(z, x, y, layers, configOptions), z, x, y)
-                .then(vector => new APIBuilder.ApiResponse(vector, VECTOR_HEADERS, 200))
-                .catch(handleError)
-        } catch (e) {
-            return handleError(e)
-        }
-    },
-    { success: { contentHandling: 'CONVERT_TO_BINARY' } },
-)
-
 api.get(
     '/',
     /* eslint-disable max-len */
     () => `
         <html>
             <head>
-            <title>Tilegarden</title>
+            <title>Tilegarden tiler</title>
             </head>
             <body>
-                <h2>Tilegarden Usage:</h2>
+                <h2>Usage:</h2>
                 <ul>
                     <li>Render raster tile at zoom/x/y: <code>/tile/{z}/{x}/{y}.png</code></li>
-                    <li>Render vector tile, rather than raster: <code>/vector/{z}/{x}/{y}</code></li>
-                    <li>UTF grid at zoom/x/y: <code>/grid/{z}/{x}/{y}?utfFields=field1,field2,field...N</code></li>
-                    <li>Filter layers: add <code>?layers=layer1,layer2,layer...N</code></li>
                 </ul>
-                <a href="https://azavea.github.io/tilegarden">Check out a demo &rArr;</a>
-                </br>
-                <a href="https://github.com/azavea/tilegarden">See on GitHub &rArr;</a>
             </body>
         </html>
     `,
