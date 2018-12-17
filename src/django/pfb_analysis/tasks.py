422ff935273c47cd059826ac5a2a34c83ed26b68
Kathryn Killebrew <kkillebrew@azavea.com>
2018-12-17 12:06:04 -0500

    Set importing status for analysis upload task
    Find task matching given UUID and set its status.

--- src/django/pfb_analysis/tasks.py
+++ src/django/pfb_analysis/tasks.py
@@ -3,7 +3,9 @@ import os
 import shutil
 import tempfile
 
-from pfb_analysis.models import AnalysisBatch
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+
+from pfb_analysis.models import AnalysisBatch, AnalysisLocalUploadTask
 from pfb_network_connectivity.utils import download_file
 from users.models import PFBUser
 
@@ -25,3 +27,15 @@ def create_batch_from_remote_shapefile(shapefile_url):
 
 def upload_local_analysis(local_upload_task_uuid):
     logging.warn('TODO: upload local analysis for task {uuid}'.format(uuid=local_upload_task_uuid))
+
+    try:
+        task = AnalysisLocalUploadTask.objects.get(uuid=local_upload_task_uuid)
+    except (ObjectDoesNotExist, ValidationError):
+        logging.error('No local upload analysis task found for {uuid}.'.format(
+                      uuid=local_upload_task_uuid))
+        return
+
+    task.status = AnalysisLocalUploadTask.Status.IMPORTING
+    task.save()
+
+    logging.info('Starting local analysis upload for {uuid}'.format(uuid=local_upload_task_uuid))
