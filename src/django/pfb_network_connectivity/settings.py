53dc065127af6ab3e11b05c78adf7107e71ec4cf
Klaas Hoekema <khoekema@azavea.com>
2019-09-09 13:18:37 -0400

    Add 'AWS_DEFAULT_ACL = None' to silence warning from 'storages'
    Explicitly sets what will be the setting in the future. I don't think we
    rely on bucket auto-creation, since we do our S3 provisioning with
    terraform. In any case, all the important buckets (i.e. staging and
    production) have already been created.

--- src/django/pfb_network_connectivity/settings.py
+++ src/django/pfb_network_connectivity/settings.py
@@ -238,6 +238,7 @@ WATCHMAN_CHECKS = (
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = os.getenv('PFB_S3_STORAGE_BUCKET',
                                     '{0}-pfb-storage-{1}'.format(DEV_USER, AWS_REGION))
+AWS_DEFAULT_ACL = None  # Override the insecure default behavior to silence the warning about it.
 AWS_QUERYSTRING_AUTH = False
 
 # Django Q
