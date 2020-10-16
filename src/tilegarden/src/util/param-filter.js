9ce2ae8e96e0208dff1d4fa08b4e1626588a3667
Kathryn Killebrew <kkillebrew@azavea.com>
2020-10-16 13:51:10 -0400

    Fix eslint Tilegarden warnings
    Use --fix to auto-apply updates to eslint rules.

--- src/tilegarden/src/util/param-filter.js
+++ src/tilegarden/src/util/param-filter.js
@@ -6,12 +6,12 @@
 const sqlString = require('sql-escape-string')
 
 // Helper function to escape col but replace outer '-s with "-s to make a delimited identifier
-const processCol = col => `"${sqlString(col).slice(1, -1)}"`
+const processCol = (col) => `"${sqlString(col).slice(1, -1)}"`
 
 // Combine parameters and values into a series of SQL conditions, ANDed
 function composeFilterQuery(filters) {
     return Object.entries(filters)
-        .map(entry => `${processCol(entry[0])} = ${sqlString(entry[1])}`)
+        .map((entry) => `${processCol(entry[0])} = ${sqlString(entry[1])}`)
         .join(' AND ')
 }
 
@@ -20,7 +20,7 @@ function composeFilterQuery(filters) {
 function applyFilterQuery(xmlJson, filterQuery) {
     xmlJson.Map.Layer.forEach((layer) => {
         // Get the <Datasource><Parameter name="table"> element, which contains the default query
-        const queryObj = layer.Datasource[0].Parameter.filter(p => p.$.name === 'table')[0]
+        const queryObj = layer.Datasource[0].Parameter.filter((p) => p.$.name === 'table')[0]
 
         // Add the filters onto it
         const query = `SELECT * FROM ${queryObj._} WHERE ${filterQuery}`
