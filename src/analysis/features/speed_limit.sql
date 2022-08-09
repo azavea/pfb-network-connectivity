a10d67a129cb61d65421860da3e8b8b286d4f965
Jacob Scarf Merrell <36001800+jacobscarfmerrell@users.noreply.github.com>
2022-08-09 13:40:57 -0400

    Convert kmph in analysis to mph (#892) (#899)
    Converted values are rounded to the nearest 5

--- src/analysis/features/speed_limit.sql
+++ src/analysis/features/speed_limit.sql
@@ -4,6 +4,13 @@
 ----------------------------------------
 UPDATE  neighborhood_ways SET speed_limit = NULL;
 
+-- convert kmph to mph and round to nearest 5
+UPDATE  neighborhood_ways
+SET     speed_limit = ROUND(substring(osm.maxspeed from '\d+')::INT / 1.609 / 5)*5
+FROM    neighborhood_osm_full_line osm
+WHERE   neighborhood_ways.osm_id = osm.osm_id
+AND     (osm.maxspeed LIKE '% kmph' OR osm.maxspeed ~ '^\d+(\.\d+)?$');
+
 UPDATE  neighborhood_ways
 SET     speed_limit = substring(osm.maxspeed from '\d+')::INT
 FROM    neighborhood_osm_full_line osm
